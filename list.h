0cdf9d0154cc3430f16e00e1ab0fb43ddb4acc01
egor79k <egor79k@yandex.ru>
2020-08-11 12:57:18 +0300

    list.h added

--- list.h
+++ list.h
@@ -25,6 +25,8 @@ private:
 	size_t sz;
 	AllocType allocator;
 
+	void destroy ();
+
 public:
 	list (const AllocType &alloc = std::allocator<ValueType> ()) :
 		head (nullptr),
@@ -33,4 +35,13 @@ public:
 		allocator (alloc)
 	{}
 
+	list (size_t size, const ValueType &val, const AllocType &alloc = std::allocator<ValueType> ());
+	
+	list (const list &other);
+	list (list &&other);
+
+	list &operator= (const list &other);
+	list &operator= (list &&other);
+
+	~list () { destroy (); }
 };
